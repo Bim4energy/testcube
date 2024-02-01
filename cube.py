716a8f08efc7036fa2fce09ed1842add5f00896a
Bim4energy <158438523+Bim4energy@users.noreply.github.com>
2024-02-01 14:08:42 +0100

    Update cube.py

--- cube.py
+++ cube.py
@@ -1,52 +1,23 @@
 import streamlit as st
 import pyvista as pv
 from stpyvista import stpyvista
-from stpyvista.utils import start_xvfb
-
-if "IS_XVFB_RUNNING" not in st.session_state:
-  start_xvfb()
-  st.session_state.IS_XVFB_RUNNING = True 
-
-st.set_page_config(page_icon="🧊", layout="wide")
-st.title("🧊 `stpyvista`")
-st.sidebar.header("Show PyVista 3D visualizations in Streamlit")
 
 ## Initialize a plotter object
-plotter = pv.Plotter(window_size=[500,400])
+plotter = pv.Plotter(window_size=[400,400])
 
 ## Create a mesh with a cube 
 mesh = pv.Cube(center=(0,0,0))
 
 ## Add some scalar field associated to the mesh
-mesh['myscalar'] = mesh.points[:, 2]*mesh.points[:, 0]
-mesh['otherscalar'] = mesh.points[:, 2]**2 + mesh.points[:, 0]
-mesh.set_active_scalars('otherscalar')
+mesh['my_scalar'] = mesh.points[:, 2] * mesh.points[:, 0]
 
 ## Add mesh to the plotter
-plotter.add_mesh(mesh, cmap='bwr', line_width=1,  label="cube")
-plotter.add_scalar_bar()
-plotter.add_title("My cube", color="black", font_size=14)
+plotter.add_mesh(mesh, scalars='my_scalar', cmap='bwr')
 
 ## Final touches
 plotter.view_isometric()
-plotter.background_color = 'lightgray'
+plotter.add_scalar_bar()
+plotter.background_color = 'white'
 
-chk = st.sidebar.checkbox("use_container_width", False)
-align = st.sidebar.select_slider("align", ["left", "center", "right"])
-
-## Pass a key to avoid re-rendering at each time something changes in the page
-stpyvista(
-    plotter, 
-    use_container_width=chk, 
-    panel_kwargs=dict(orientation_widget = True),
-    horizontal_align=align
-)
-
-## Add something else below
-st.markdown("Jello there"*50)
-
-cols = st.columns([1,2,1], gap="small")
-
-for col in cols:
-    with col:
-        stpyvista(plotter, use_container_width=chk, bokeh_resources="CDN")
+## Pass a key to avoid re-rendering at each page change
+stpyvista(plotter, key="pv_cube")
